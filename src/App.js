6ea0e33010269fec080b4eb3c6362cc3a7811ddb
duchieu <duchieu.ctk41@gmail.com>
2020-11-19 14:02:09 +0700

    [1.0.0] complete video 4.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,47 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
+import person from './Person/Person';
 
 class App extends Component {
   state={
     persons: [
-      {name:'hieu', age: 28},
-      {name:'huyen', age: 18},
-      {name:'hoa', age: 38},
+      {id:'abc', name:'hieu', age: 28},
+      {id:'bcd', name:'huyen', age: 18},
+      {id:'cde', name:'hoa', age: 38},
     ],
     showPersons: false
   }
 
+  nameChangeHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    // const person = Object.assign({}, this.state.persons[personIndex]);
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState( {persons: persons} )
+  };
+
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
     this.setState({showPersons: !doesShow});
+  }
+
+  deletePersonHandler = (personIndex) => {
+    // const persons = this.state.persons.slice();
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons});
   }
 
 
@@ -26,6 +53,25 @@ class App extends Component {
       padding: '8px',
       cursor: 'pointer'
     }
+
+    let persons = null;
+
+    if(this.state.showPersons) {
+      persons = (
+          <div>
+            {this.state.persons.map((person, index) => {
+              return <Person 
+                click={() => this.deletePersonHandler(index)}
+                name={person.name}
+                age={person.age}
+                key={person.id}
+                changed={(event) => this.nameChangeHandler(event, person.id)}
+              />
+            })}
+          </div>
+      )
+
+    }
       return (
         <div className="App">
          <h1>Hi, i'm Hieu</h1>
@@ -36,16 +82,7 @@ class App extends Component {
          >
          Toggle persons  
          </button>
-         {
-          this.state.showPersons === true ? 
-          <div>
-         <Person name={this.state.persons[0].name}
-         age={this.state.persons[0].age}
-         />
-         <Person name={this.state.persons[1].name}
-         age={this.state.persons[1].age}
-         />
-         </div> : null}
+         {persons}
         </div> 
       );
     }
