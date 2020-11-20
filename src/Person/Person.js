0ef0737f9378b7148cf70d97b56a5432d09c9d9a
duchieu <duchieu.ctk41@gmail.com>
2020-11-20 21:57:06 +0700

    [1.0.0] add  module css eject in person.js - video 11/5.

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,26 +1,13 @@
 import React from 'react';
-// import Radium from 'radium';
-import styled from 'styled-components';
-
-const StyleDiv = styled.div`
-        margin: 16px auto;
-        padding: 16px;
-        border: 1px solid black;
-
-        @media (min-width: 600px) {
-            background-color: black;
-            color: white;
-        }
-    `;
+import classes from './Person.css';
 
 const person = (props) => {
 
     return (
-        // <div style={style} className="Person">
-        <StyleDiv>
+        <div className={classes.Person}>
             <p onClick={props.click}>Hi, i'm {props.name}, i'm {props.age} years old</p>
             <input type="text" onChange={props.changed}></input>
-        </StyleDiv>
+        </div>
     )
 }
 
