e7cb919d40f9386cf0e2ae10ef9596f5157e9ac5
duchieu <duchieu.ctk41@gmail.com>
2020-11-23 11:04:34 +0700

    [1.0.0] useEffect to functional component -  video 10,11/7.

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import classes from './Cockpit.css';
 
 const cockpit = (props) => {
+    useEffect(() => {
+       console.log('[Cockpit.js] useEffect');
+       // Http request
+       setTimeout(() => {
+        alert('Saved data to cloud');
+       }, 1000);
+    }, []);
+
+    // useEffect();
+
     const assignClasses = [];
     let btnClass = '';
     if (props.showPersons) {
