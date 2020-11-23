024c5026a935f7699eeda999ab60a092d14300ca
duchieu <duchieu.ctk41@gmail.com>
2020-11-23 21:52:25 +0700

    [1.0.0] rerendering Ajacident JSX element - video 19/7.

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import classes from './Person.css';
 
+import Aux from '../../../hoc/Aux';
+
 class Person extends Component {
     render() {
         console.log('[Person.js] rendering...');
         return (
-            <div className={classes.Person}>
-                <p onClick={this.props.click}>Hi, i'm {this.props.name}, i'm {this.props.age} years old</p>
-                <input type="text" onChange={this.props.changed}></input>
-            </div>
+            <Aux>
+                <p 
+                    key="i1"
+                onClick={this.props.click}>Hi, i'm {this.props.name}, i'm {this.props.age} years old</p>
+                <input 
+                    key="i2"
+                type="text" onChange={this.props.changed}></input>
+            </Aux>
         )
     }
 }
