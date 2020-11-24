59d98448e8ca22efada90ce476a0fbdaee2a2f80
duchieu <duchieu.ctk41@gmail.com>
2020-11-24 19:48:01 +0700

    [1.0.0] using ref - video 27/7.

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -5,6 +5,16 @@ import classes from './Person.css';
 import Aux from '../../../hoc/Aux';
 
 class Person extends Component {
+    constructor(props) {
+        super(props);
+        this.inputElementRef = React.createRef();
+    }
+    
+    componentDidMount() {
+        // this.inputElementRef.focus();
+        this.inputElementRef.current.focus();
+    }
+
     render() {
         console.log('[Person.js] rendering...');
         return (
@@ -14,7 +24,10 @@ class Person extends Component {
                 onClick={this.props.click}>Hi, i'm {this.props.name}, i'm {this.props.age} years old</p>
                 <input 
                     key="i2"
-                type="text" onChange={this.props.changed} value={this.props.name}></input>
+                    // ref={(inputEl) => {this.inputElement = inputEl}}
+                    ref={this.inputElementRef}
+                type="text" onChange={this.props.changed} value={this.props.name}
+                ></input>
             </Aux>
         )
     }
