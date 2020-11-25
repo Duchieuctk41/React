f4aaa94ec267949fb2a9e197d2961ed2ae064f93
duchieu <duchieu.ctk41@gmail.com>
2020-11-25 18:52:21 +0700

    [1.0.0] create folder BurgerBuilder & set css in Layout - 9/8.

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Aux from '../../hoc/Aux';
+
+const burgerBuilder = () => {
+    return (
+        <Aux>
+        <div>Burger</div>
+        <div>Burger control</div>
+        </Aux>
+    );
+} 
+
+export default burgerBuilder;
