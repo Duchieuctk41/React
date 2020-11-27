410547eba1a345c876d980e62b4d960d0a78ee9b
duchieu <duchieu.ctk41@gmail.com>
2020-11-27 20:32:15 +0700

    [1.3.0] fix fault 05.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './containers/App';
+import App from './App';
 import './index.css';
 
 ReactDOM.render(
-  <App appTitle="Person Manager"/>,
+  <App />,
   document.getElementById('root')
 );
