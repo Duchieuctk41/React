09e495fb7f049053d5858aa85d1bf4b1ccb48b6d
duchieu <duchieu.ctk41@gmail.com>
2020-11-28 16:50:03 +0700

    31/8 [1.0.0] adjust responsive.

--- src/components/Navigation/SideDraw/SideDrawer.js
+++ src/components/Navigation/SideDraw/SideDrawer.js
@@ -1,14 +1,16 @@
 import React from 'react';
 
-import Logo from '../Logo/Logo';
-import NavigationItems from '../Navigation/NavigationItems/NavigationItems';
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.css';
 
-const sideDrawer = (props) => {
+const sideDrawer = ( props ) => {
     //...
     return (
         <div className={classes.SideDrawer}>
-            <Logo />
+            <div className={classes.Logo}>
+                <Logo />
+            </div>
             <nav>
                 <NavigationItems />
             </nav>
