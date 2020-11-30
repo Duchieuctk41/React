cf4416b21294ba797034e2f277a7227c48404a64
duchieu <duchieu.ctk41@gmail.com>
2020-11-30 21:42:17 +0700

    [1.0.0] loz me

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,35 +1,15 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 
 import classes from './FullPost.css';
 
 class FullPost extends Component {
-    state = {
-        loadedPost: null
-    }
-
-    componentDidMount () {
-        if (this.props.id) {
-            axios.get('http://jsonplaceholder.typicode.com/posts/' + this.props.id)
-                .then(response => {
-                    // console.log(response);
-                    this.setState({loadedPost: response.data});
-                });
-        }
-        
-    }
     render () {
-
-        let post = <p>Please select a Post!</p>;
+        let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
         if (this.props.id) {
-            post = <p>Loading...!</p>;
-        }
-        
-        if (this.state.loadedPost) {
             post = (
                 <div className={classes.FullPost}>
-                    <h1>{this.loadedPost.title}</h1>
-                    <p>{this.loadedPost.content}</p>
+                    <h1>Title</h1>
+                    <p>Content</p>
                     <div className={classes.Edit}>
                         <button className={classes.Delete}>Delete</button>
                     </div>
